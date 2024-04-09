ea7978cbbba5898b85587a77daa702f9e6994c91
Nitin-Joshi-NJ <121297538+Nitin-Joshi-NJ@users.noreply.github.com>
2024-04-09 16:38:17 +0530

    Update Alerts.js

--- app/components/Alerts.js
+++ app/components/Alerts.js
@@ -1,1 +1,16 @@
+import React, { useEffect, useState } from "react";
+
+export default function Alerts({ type, message }) {
+  return (
+    <>
+        <div
+          className={`container mx-auto mt-2 alert alert-${type} text-center bg-green-200 p-1`}
+        >
+          <span className="text-green-600 font-extrabold text-lg">
+            {message}
+          </span>
+        </div>
+    </>
+  );
+}
 
